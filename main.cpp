f94266a2adb7c68696e9796e139ac1e151215f0f
jczuchra <czuchra.jakub@gmail.com>
2019-11-10 19:56:29 +0100

    Add libraries

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include "libraries/add/add.h"
 
 int main() {
     std::cout << "Hello_world\n";
+    std::cout << "The sum of 3 and 4 is:" << add(3,4) << "\n";
     return 0;
 }
