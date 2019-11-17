39b7f3fd0b0bf8dfd043ff256087126932d8c081
gauasz <n.galaszewicz@wp.pl>
2019-11-17 12:59:34 +0100

    test - add all

---
+++ CMakeFiles/dadm.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/dadm2019/Code/dadm-cpp/src/helpers/add.cpp" "/home/dadm2019/Code/dadm-cpp/CMakeFiles/dadm.dir/src/helpers/add.cpp.o"
+  "/home/dadm2019/Code/dadm-cpp/src/main.cpp" "/home/dadm2019/Code/dadm-cpp/CMakeFiles/dadm.dir/src/main.cpp.o"
+  "/home/dadm2019/Code/dadm-cpp/src/view/window.cpp" "/home/dadm2019/Code/dadm-cpp/CMakeFiles/dadm.dir/src/view/window.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/dadm.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dadm.dir/src/main.cpp.o"
+  "CMakeFiles/dadm.dir/src/helpers/add.cpp.o"
+  "CMakeFiles/dadm.dir/src/view/window.cpp.o"
+  "dadm.pdb"
+  "dadm"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dadm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
