0b324d218d8fd2f21e4c32e94b56c54f45539276
typerw <typerwiktoria@gmail.com>
2020-01-11 14:26:02 +0100

    dodanie cmake-build-debug/

---
+++ cmake-build-debug/CMakeFiles/dadm.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/Typer/Desktop/projekt/dadm-cpp-master/_src/Source2.cpp" "/Users/Typer/Desktop/projekt/dadm-cpp-master/cmake-build-debug/CMakeFiles/dadm.dir/_src/Source2.cpp.o"
+  "/Users/Typer/Desktop/projekt/dadm-cpp-master/_src/_main.cpp" "/Users/Typer/Desktop/projekt/dadm-cpp-master/cmake-build-debug/CMakeFiles/dadm.dir/_src/_main.cpp.o"
+  "/Users/Typer/Desktop/projekt/dadm-cpp-master/_src/helpers/add.cpp" "/Users/Typer/Desktop/projekt/dadm-cpp-master/cmake-build-debug/CMakeFiles/dadm.dir/_src/helpers/add.cpp.o"
+  "/Users/Typer/Desktop/projekt/dadm-cpp-master/_src/main.cpp" "/Users/Typer/Desktop/projekt/dadm-cpp-master/cmake-build-debug/CMakeFiles/dadm.dir/_src/main.cpp.o"
+  "/Users/Typer/Desktop/projekt/dadm-cpp-master/_src/model.cpp" "/Users/Typer/Desktop/projekt/dadm-cpp-master/cmake-build-debug/CMakeFiles/dadm.dir/_src/model.cpp.o"
+  "/Users/Typer/Desktop/projekt/dadm-cpp-master/_src/view/window.cpp" "/Users/Typer/Desktop/projekt/dadm-cpp-master/cmake-build-debug/CMakeFiles/dadm.dir/_src/view/window.cpp.o"
+  "/Users/Typer/Desktop/projekt/dadm-cpp-master/src/ECGAnalyzer/datafile.cpp" "/Users/Typer/Desktop/projekt/dadm-cpp-master/cmake-build-debug/CMakeFiles/dadm.dir/src/ECGAnalyzer/datafile.cpp.o"
+  "/Users/Typer/Desktop/projekt/dadm-cpp-master/src/ECGAnalyzer/dataloader.cpp" "/Users/Typer/Desktop/projekt/dadm-cpp-master/cmake-build-debug/CMakeFiles/dadm.dir/src/ECGAnalyzer/dataloader.cpp.o"
+  "/Users/Typer/Desktop/projekt/dadm-cpp-master/src/ECGAnalyzer/ecganalyzercontroller.cpp" "/Users/Typer/Desktop/projekt/dadm-cpp-master/cmake-build-debug/CMakeFiles/dadm.dir/src/ECGAnalyzer/ecganalyzercontroller.cpp.o"
+  "/Users/Typer/Desktop/projekt/dadm-cpp-master/src/ECGAnalyzer/main.cpp" "/Users/Typer/Desktop/projekt/dadm-cpp-master/cmake-build-debug/CMakeFiles/dadm.dir/src/ECGAnalyzer/main.cpp.o"
+  "/Users/Typer/Desktop/projekt/dadm-cpp-master/src/ECGAnalyzer/mainwindow.cpp" "/Users/Typer/Desktop/projekt/dadm-cpp-master/cmake-build-debug/CMakeFiles/dadm.dir/src/ECGAnalyzer/mainwindow.cpp.o"
+  "/Users/Typer/Desktop/projekt/dadm-cpp-master/src/ECGAnalyzer/module.cpp" "/Users/Typer/Desktop/projekt/dadm-cpp-master/cmake-build-debug/CMakeFiles/dadm.dir/src/ECGAnalyzer/module.cpp.o"
+  "/Users/Typer/Desktop/projekt/dadm-cpp-master/src/ECGAnalyzer/modulebaseline.cpp" "/Users/Typer/Desktop/projekt/dadm-cpp-master/cmake-build-debug/CMakeFiles/dadm.dir/src/ECGAnalyzer/modulebaseline.cpp.o"
+  "/Users/Typer/Desktop/projekt/dadm-cpp-master/src/ECGAnalyzer/viewmodule.cpp" "/Users/Typer/Desktop/projekt/dadm-cpp-master/cmake-build-debug/CMakeFiles/dadm.dir/src/ECGAnalyzer/viewmodule.cpp.o"
+  "/Users/Typer/Desktop/projekt/dadm-cpp-master/src/ECGAnalyzer/viewmodulebaseline.cpp" "/Users/Typer/Desktop/projekt/dadm-cpp-master/cmake-build-debug/CMakeFiles/dadm.dir/src/ECGAnalyzer/viewmodulebaseline.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/dadm.dir/cmake_clean.cmake
@@ -0,0 +1,24 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dadm.dir/_src/Source2.cpp.o"
+  "CMakeFiles/dadm.dir/_src/_main.cpp.o"
+  "CMakeFiles/dadm.dir/_src/helpers/add.cpp.o"
+  "CMakeFiles/dadm.dir/_src/main.cpp.o"
+  "CMakeFiles/dadm.dir/_src/model.cpp.o"
+  "CMakeFiles/dadm.dir/_src/view/window.cpp.o"
+  "CMakeFiles/dadm.dir/src/ECGAnalyzer/datafile.cpp.o"
+  "CMakeFiles/dadm.dir/src/ECGAnalyzer/dataloader.cpp.o"
+  "CMakeFiles/dadm.dir/src/ECGAnalyzer/ecganalyzercontroller.cpp.o"
+  "CMakeFiles/dadm.dir/src/ECGAnalyzer/main.cpp.o"
+  "CMakeFiles/dadm.dir/src/ECGAnalyzer/mainwindow.cpp.o"
+  "CMakeFiles/dadm.dir/src/ECGAnalyzer/module.cpp.o"
+  "CMakeFiles/dadm.dir/src/ECGAnalyzer/modulebaseline.cpp.o"
+  "CMakeFiles/dadm.dir/src/ECGAnalyzer/viewmodule.cpp.o"
+  "CMakeFiles/dadm.dir/src/ECGAnalyzer/viewmodulebaseline.cpp.o"
+  "dadm"
+  "dadm.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dadm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
