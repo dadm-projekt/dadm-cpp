02143b2eb34b7ef39e597cff53e780c34b6d5596
Ada <szymczykada1@gmail.com>
2020-01-15 21:49:56 +0100

    checking

--- src/ECGAnalyzer/modulebaseline.cpp
+++ src/ECGAnalyzer/modulebaseline.cpp
@@ -13,9 +13,9 @@ ModuleBaseline::~ModuleBaseline()
 
 void ModuleBaseline ::AnalyzeSignal()
 {
-    //tu powinna zostac wywolana metoda w zaleznosci od tego ktory filtr wybral uzytkownik
+    //tu powinna zostac wywolana metoda w zaleznosci od tego ktory filtr wybral uzytkownik - TEST
 }
-
+g
 void ModuleBaseline::ButterworthFilter()
 {
 
