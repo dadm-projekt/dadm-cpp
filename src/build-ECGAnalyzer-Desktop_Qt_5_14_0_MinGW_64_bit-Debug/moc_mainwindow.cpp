6ea0593967120e979ea3c5771937c83e7ae5a18e
jczuchra <czuchra.jakub@gmail.com>
2020-01-26 20:18:56 +0100

    Improve look of GUI

--- src/build-ECGAnalyzer-Desktop_Qt_5_14_0_MinGW_64_bit-Debug/moc_mainwindow.cpp
+++ src/build-ECGAnalyzer-Desktop_Qt_5_14_0_MinGW_64_bit-Debug/moc_mainwindow.cpp
@@ -22,8 +22,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_MainWindow_t {
-    QByteArrayData data[3];
-    char stringdata0[34];
+    QByteArrayData data[12];
+    char stringdata0[192];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -34,10 +34,24 @@ static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
     {
 QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
 QT_MOC_LITERAL(1, 11, 21), // "on_pushButton_clicked"
-QT_MOC_LITERAL(2, 33, 0) // ""
+QT_MOC_LITERAL(2, 33, 0), // ""
+QT_MOC_LITERAL(3, 34, 23), // "on_pushButton_2_clicked"
+QT_MOC_LITERAL(4, 58, 30), // "on_tableWidget_3_cellActivated"
+QT_MOC_LITERAL(5, 89, 3), // "row"
+QT_MOC_LITERAL(6, 93, 6), // "column"
+QT_MOC_LITERAL(7, 100, 32), // "on_comboBox_2_currentTextChanged"
+QT_MOC_LITERAL(8, 133, 4), // "arg1"
+QT_MOC_LITERAL(9, 138, 23), // "on_comboBox_2_activated"
+QT_MOC_LITERAL(10, 162, 23), // "on_comboBox_3_activated"
+QT_MOC_LITERAL(11, 186, 5) // "index"
 
     },
-    "MainWindow\0on_pushButton_clicked\0"
+    "MainWindow\0on_pushButton_clicked\0\0"
+    "on_pushButton_2_clicked\0"
+    "on_tableWidget_3_cellActivated\0row\0"
+    "column\0on_comboBox_2_currentTextChanged\0"
+    "arg1\0on_comboBox_2_activated\0"
+    "on_comboBox_3_activated\0index"
 };
 #undef QT_MOC_LITERAL
 
@@ -47,7 +61,7 @@ static const uint qt_meta_data_MainWindow[] = {
        8,       // revision
        0,       // classname
        0,    0, // classinfo
-       1,   14, // methods
+       6,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -55,10 +69,20 @@ static const uint qt_meta_data_MainWindow[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   19,    2, 0x08 /* Private */,
+       1,    0,   44,    2, 0x08 /* Private */,
+       3,    0,   45,    2, 0x08 /* Private */,
+       4,    2,   46,    2, 0x08 /* Private */,
+       7,    1,   51,    2, 0x08 /* Private */,
+       9,    1,   54,    2, 0x08 /* Private */,
+      10,    1,   57,    2, 0x08 /* Private */,
 
  // slots: parameters
     QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int, QMetaType::Int,    5,    6,
+    QMetaType::Void, QMetaType::QString,    8,
+    QMetaType::Void, QMetaType::QString,    8,
+    QMetaType::Void, QMetaType::Int,   11,
 
        0        // eod
 };
@@ -70,10 +94,14 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         Q_UNUSED(_t)
         switch (_id) {
         case 0: _t->on_pushButton_clicked(); break;
+        case 1: _t->on_pushButton_2_clicked(); break;
+        case 2: _t->on_tableWidget_3_cellActivated((*reinterpret_cast< int(*)>(_a[1])),(*reinterpret_cast< int(*)>(_a[2]))); break;
+        case 3: _t->on_comboBox_2_currentTextChanged((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        case 4: _t->on_comboBox_2_activated((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        case 5: _t->on_comboBox_3_activated((*reinterpret_cast< int(*)>(_a[1]))); break;
         default: ;
         }
     }
-    Q_UNUSED(_a);
 }
 
 QT_INIT_METAOBJECT const QMetaObject MainWindow::staticMetaObject = { {
@@ -105,13 +133,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 1)
+        if (_id < 6)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 1;
+        _id -= 6;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 1)
+        if (_id < 6)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 1;
+        _id -= 6;
     }
     return _id;
 }
